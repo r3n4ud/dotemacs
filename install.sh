3793daa35aafa3d2f6cce14d8adaad3838db4bb0
Renaud AUBIN <renaud.aubin@nibua-r.org>
2011-11-27 17:39:27 +0100

    Add a convenience bash installation script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+INITIAL_DIR=$(pwd)
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+SCRIPT_BASE_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+INSTALL_DIR=$SCRIPT_BASE_DIR/.emacs.d/site-lisp
+
+echo "-- Move to source directory ${SCRIPT_BASE_DIR}/install"
+cd $SCRIPT_BASE_DIR/install
+
+for i in android-mode auto-complete
+do
+    echo "--- Install ${i}"
+    cd $i
+    [ ! -d $INSTALL_DIR/$i ] && mkdir $INSTALL_DIR/$i
+    make install DIR=$INSTALL_DIR/$i
+    cd ..
+done
+
+cd $INITIAL_DIR
