22a1e02923d7d3218eb89365dc49ebffdeba1ed3
Renaud AUBIN <root@renaud.io>
2013-12-23 20:04:14 +0100

    Upgrade yasnippet and the related configuration

--- .emacs.d/init-yasnippet.el
+++ .emacs.d/init-yasnippet.el
@@ -1,7 +1,9 @@
 (require 'yasnippet)
 ;; Don't use tab as trigger key
 (setq yas/trigger-key (kbd "C-c C-t"))
-(setq yas/snippet-dirs '("~/.emacs.d/site-lisp/snippets" "~/.emacs.d/site-lisp/yasnippet/snippets" "~/.emacs.d/site-lisp/yasnippet/extras/imported"))
+(setq yas/snippet-dirs '("~/.emacs.d/site-lisp/snippets"
+                         "~/.emacs.d/site-lisp/yasnippet/yasmate/snippets"
+                         "~/.emacs.d/site-lisp/yasnippet/snippets"))
 (yas/global-mode 1)
 
 (provide 'init-yasnippet)
