b6f57ebdf5a43ef904437da25bc3b8c23440b35f
Renaud AUBIN <root@renaud.io>
2023-02-08 15:01:35 +0100

    Update

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -10,17 +10,22 @@
  '(browse-url-firefox-new-window-is-tab t)
  '(browse-url-firefox-program "~/.local/opt/firefox/firefox")
  '(browse-url-generic-program (executable-find "chromium"))
- '(clang-format-executable
-   "~/.local/clang+llvm-11.0.1-x86_64-linux-gnu-ubuntu-16.04/bin/clang-format")
+ '(clang-format-executable "~/.local/clang/bin/clang-format")
+ '(flycheck-c/c++-clang-executable "~/.local/clang/bin/clang++")
+ '(flycheck-c/c++-clang-tidy-executable "~/.local/clang/bin/clang-tidy")
+ '(flycheck-c/c++-gcc-executable "/opt/gcc-8.3.0/bin/gcc")
  '(flycheck-c/c++-googlelint-executable "~/.local/bin/cpplint")
+ '(lsp-java-format-settings-url
+   "file:///REPLACEWITHHOME/.emacs.d/eclipse-java-google-style.xml")
+ '(lsp-java-java-path "/usr/lib/jvm/jdk-11/bin/java")
+ '(lsp-java-server-install-dir "~/.local/opt/jdt-language-server-1.5.0/")
+ '(magit-lfs-git-lfs-executable "~/.local/bin/git-lfs")
  '(nxml-slash-auto-complete-flag t)
  '(package-selected-packages
-   '(company-box pyenv-mode lsp-ui lsp-java flycheck-clang-analyzer flycheck-clang-tidy mu4e-alert csv-mode auctex org-mime adoc-mode clang-format+ clang-format modern-cpp-font-lock yasnippet-snippets company-irony-c-headers ess-view ess-smart-equals ess flycheck-rust 2048-game plantuml-mode protobuf-mode flycheck-irony cmake-font-lock yasnippet company-auctex lua-mode yaml-mode racer cargo rust-mode use-package undo-tree smex smartparens magit git-gutter company-quickhelp))
+   '(yard-mode company-inf-ruby company-nginx elisp-refs helpful ein rustic counsel company-box pyenv-mode lsp-ui lsp-java flycheck-clang-analyzer flycheck-clang-tidy mu4e-alert csv-mode auctex org-mime adoc-mode clang-format+ clang-format modern-cpp-font-lock yasnippet-snippets company-irony-c-headers ess-view ess-smart-equals ess 2048-game plantuml-mode protobuf-mode flycheck-irony cmake-font-lock yasnippet company-auctex lua-mode yaml-mode rust-mode use-package undo-tree smex smartparens git-gutter company-quickhelp))
  '(prettify-symbols-unprettify-at-point t)
- '(safe-local-variable-values '((whitespace-line-column . 80)))
- '(show-paren-mode t)
- '(smtpmail-smtp-server "localhost")
- '(smtpmail-smtp-service 25))
+ '(rust-rustfmt-bin "~/.cargo/bin/rustfmt")
+ '(safe-local-variable-values '((whitespace-line-column . 80))))
 (provide 'custom)
 ;;; custom.el ends here
 (custom-set-faces
